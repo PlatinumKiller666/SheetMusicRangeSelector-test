ba3769634a943b6914063c0696a9fe002c525308
Kirill Zolotarev <kirbuldog@gmail.com>
2025-12-30 11:14:53 +0300

    first commit

---
+++ SheetMusicRangeSelector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SheetMusicRangeSelector
+//
+//  Created by Kirill Zolotarev on 30.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SheetMusicRangeSelector/SheetMusicRangeSelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetMusicRangeSelectorApp.swift
+//  SheetMusicRangeSelector
+//
+//  Created by Kirill Zolotarev on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct SheetMusicRangeSelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
